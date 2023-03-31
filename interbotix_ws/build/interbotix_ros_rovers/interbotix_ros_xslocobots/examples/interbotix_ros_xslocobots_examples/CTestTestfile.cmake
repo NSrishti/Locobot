398e96f4b2b5a8ac387dcf8709fbe17d63c7507f
NSrishti <srishtinigam97@gmail.com>
2023-03-31 21:24:30 +0200

    hi

--- interbotix_ws/build/interbotix_ros_rovers/interbotix_ros_xslocobots/examples/interbotix_ros_xslocobots_examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/srishti/interbotix_ws/src/interbotix_ros_rovers/interbotix_ros_xslocobots/examples/interbotix_ros_xslocobots_examples
-# Build directory: /home/srishti/interbotix_ws/build/interbotix_ros_rovers/interbotix_ros_xslocobots/examples/interbotix_ros_xslocobots_examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
